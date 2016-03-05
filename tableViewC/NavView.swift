34f36b981a10c340762fc3c818702a56d5ab7737
blues <blues@blues.com>
2016-03-05 13:48:24 +0800

    pageView bug fix

---
+++ tableViewC/NavView.swift
@@ -0,0 +1,21 @@
+//
+//  NavView.swift
+//  tableViewC
+//
+//  Created by blues on 16/3/5.
+//  Copyright © 2016年 blues. All rights reserved.
+//
+
+import UIKit
+
+class NavView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ tableViewC/e_tableView_HomeController.swift
@@ -0,0 +1,9 @@
+//
+//  e_tableView_HomeController.swift
+//  tableViewC
+//
+//  Created by blues on 16/3/5.
+//  Copyright © 2016年 blues. All rights reserved.
+//
+
+import Foundation
