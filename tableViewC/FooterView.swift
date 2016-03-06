8874b4433795f5c55fe7aa8b310e956ada66c0af
blues <blues@blues.com>
2016-03-06 11:56:39 +0800

    完成home的布局

---
+++ tableViewC/FooterView.swift
@@ -0,0 +1,21 @@
+//
+//  FooterView.swift
+//  tableViewC
+//
+//  Created by blues on 16/3/6.
+//  Copyright © 2016年 blues. All rights reserved.
+//
+
+import UIKit
+
+class FooterView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
