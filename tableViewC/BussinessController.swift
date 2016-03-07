70a8f9c1b596b90f3c18c3fd022f6a5257997c58
blues <blues@blues.com>
2016-03-07 17:21:26 +0800

    完成Bussiness页和配色

--- tableViewC/BussinessController.swift
+++ tableViewC/BussinessController.swift
@@ -9,5 +9,27 @@
 import UIKit 
 
 class BussinessController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        self.tableView.registerNib(UINib(nibName: "ImageCell", bundle: mainBundle), forCellReuseIdentifier: "ImageCell")
+    }
 }
+
+extension BussinessController:UITableViewDataSource{
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let imageCell = tableView.dequeueReusableCellWithIdentifier("ImageCell") as! TableViewCell
+        imageCell.imageV.image = DataAPI.shareInstance.getImages()[3]
+        
+        return imageCell
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return tableView.frame.height / 4
+    }
+}
+
+extension BussinessController:UITableViewDelegate{}
---
+++ tableViewC/TabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarController.swift
+//  tableViewC
+//
+//  Created by blues on 16/3/7.
+//  Copyright © 2016年 blues. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        tabBar.backgroundColor = UIColor.whiteColor()
+        tabBar.tintColor = UIColor(red: 86 / 255, green: 189 / 255, blue: 171 / 255, alpha: 1)
+    }
+}
